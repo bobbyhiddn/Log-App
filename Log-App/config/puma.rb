94eb88ddbc69ed68a22e81a8ac89ccd25473f578
Micah Longmire <mlmicahlongmire@gmail.com>
2022-12-11 08:00:44 -0700

    Working on things

--- Log-App/config/puma.rb
+++ Log-App/config/puma.rb
@@ -1,3 +1,6 @@
+#Change this to the remote server's IP
+#bind 'tcp://0.0.0.0:3000'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
