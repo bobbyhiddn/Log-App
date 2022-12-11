9a0beb7ed24b7214f203380d276ad7958cc2caf3
Micah <mlmicahlongmire@gmail.com>
2022-12-11 14:19:01 +0000

    We have liftoff. Making the tickets controller.

---
+++ Log-App/app/controllers/tickets_controller.rb
@@ -0,0 +1,20 @@
+class TicketsController < ApplicationController
+  def create
+    # Create a new ticket using the submitted data
+    ticket = Ticket.new(params[:ticket])
+
+    # Save the ticket to the database
+    if ticket.save
+      # If the ticket is successfully saved, redirect the user to the ticket page
+      redirect_to ticket_path(ticket)
+    else
+      # If the ticket is not successfully saved, display an error message
+      flash[:error] = "There was a problem creating your ticket"
+    end
+  end
+
+  def show
+    # Retrieve the specified ticket from the database
+    @ticket = Ticket.find(params[:id])
+  end
+end
